fb13d306f8294a5be6c32ccc4556c3a02450fb6f
Aria Dolatabadian <74664712+Aria-Dolatabadian@users.noreply.github.com>
2023-06-29 14:30:05 +0800

    Update Code.py

--- Code.py
+++ Code.py
@@ -16,3 +16,8 @@ def calculate_n50(lengths):
 contig_lengths = [756,100,800,1000]
 n50 = calculate_n50(contig_lengths)
 print("N50 value:", n50)
+
+
+#To calculate the N50 value, the contigs or reads are first arranged in descending order based on their lengths. 
+#Then, the lengths of the contigs or reads are summed cumulatively until the cumulative length reaches or exceeds half of the total assembly length. 
+#The N50 value is the length of the contig or read at this point.
